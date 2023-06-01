7d592229b827b883588897cff8f856d9acf01b48
Phuclvph20069 <phuclvph20069@fpt.edu.vn>
2023-06-01 20:25:16 +0700

    divide layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
+import { DashboardComponent } from './page/admin/dashboard/dashboard.component';
+import { HomePageComponent } from './page/home-page/home-page.component';
+import { PageNotFoundComponent } from './page/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BaseLayoutComponent,
+    children: [{ path: '', component: HomePageComponent }],
+  },
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+    ],
+  },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterAdminComponent } from './components/layout/admin/footer-admin/footer-admin.component';
+import { HeaderAdminComponent } from './components/layout/admin/header-admin/header-admin.component';
+import { FooterComponent } from './components/layout/base/footer/footer.component';
+import { HeaderComponent } from './components/layout/base/header/header.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
+import { DashboardComponent } from './page/admin/dashboard/dashboard.component';
+import { ProductsComponent } from './page/admin/products/products.component';
+import { HomePageComponent } from './page/home-page/home-page.component';
+import { PageNotFoundComponent } from './page/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AdminLayoutComponent,
+    BaseLayoutComponent,
+    DashboardComponent,
+    ProductsComponent,
+    HomePageComponent,
+    PageNotFoundComponent,
+    HeaderAdminComponent,
+    FooterAdminComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
