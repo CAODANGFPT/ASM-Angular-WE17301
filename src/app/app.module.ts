8e93027fbbe6ca3f72d855bd2799951806d3db48
hai20112003 <nguyenhai20112003@gmail.com>
2023-06-04 02:29:16 +0700

    AAW-6 feature: fix scss login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { HeaderComponent } from './components/layout/base/header/header.componen
 import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
 import { DashboardComponent } from './page/admin/dashboard/dashboard.component';
-import { HomePageComponent } from './page/home-page/home-page.component';
+import { HomePageComponent } from './page/base/home-page/home-page.component';
 import { PageNotFoundComponent } from './page/page-not-found/page-not-found.component';
 import { ProductAddComponent } from './page/admin/products/product-add/product-add.component';
 import { ProductListComponent } from './page/admin/products/product-list/product-list.component';
