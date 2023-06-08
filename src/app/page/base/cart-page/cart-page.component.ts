67fceac28f2ba924b84b12ff8ce1bfb68c4491f0
Thuc241203 <thucngo241203@gmail.com>
2023-06-08 14:14:44 +0700

    fix: scss cart page refs: AAW-8

--- src/app/page/base/cart-page/cart-page.component.ts
+++ src/app/page/base/cart-page/cart-page.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+
+interface ICard{
+  products: IProduct[];
+  total: number;
+}
 interface IProduct {
   id: number;
   name: string;
   price: number;
+  priceNew: number;
   image: string;
-  quantity?: number;
+  quantity: number;
+  quantityProducts: number;
 }
 @Component({
   selector: 'app-cart-page',
@@ -12,37 +19,60 @@ interface IProduct {
   styleUrls: ['./cart-page.component.scss'],
 })
 export class CartPageComponent {
-  product: IProduct[] = [
-    {
-      id: 1,
-      name: 'product A',
-      price: 1000,
-      image:
-        'https://bizweb.dktcdn.net/thumb/compact/100/091/132/products/5-min-a5bb63b5-5f5e-4109-ae48-b6f4e3c5a3aa.jpg',
-      quantity: 1,
-    },
-    {
-      id: 2,
-      name: 'product B',
-      price: 1000,
-      image:
-        'https://bizweb.dktcdn.net/thumb/compact/100/091/132/products/5-min-a5bb63b5-5f5e-4109-ae48-b6f4e3c5a3aa.jpg',
-      quantity: 1,
-    },
-  ];
+  openCart: boolean = true;
+
+  card: ICard = {
+    products: [
+      {
+        id: 1,
+        name: 'product A',
+        price: 1000,
+        priceNew: 1000,
+        image:
+          'https://bizweb.dktcdn.net/thumb/compact/100/091/132/products/5-min-a5bb63b5-5f5e-4109-ae48-b6f4e3c5a3aa.jpg',
+        quantity: 1,
+        quantityProducts: 11,
+      },
+      {
+        id: 2,
+        name: 'product B',
+        price: 11,
+        priceNew: 11,
+        image:
+          'https://bizweb.dktcdn.net/thumb/compact/100/091/132/products/5-min-a5bb63b5-5f5e-4109-ae48-b6f4e3c5a3aa.jpg',
+        quantity: 1,
+        quantityProducts: 100,
+      },
+    ],
+    total: 1011,
+  };
+
+  calculateTotal() {
+    this.card.total = this.card.products.reduce(
+      (total, product) => total + product.priceNew,
+      0
+    );
+  }
+
   minus(id: number) {
-    const product = this.product.find((item) => item.id === id);
+    const product = this.card.products.find((item) => item.id === id);
     if (product && product.quantity && product.quantity > 1) {
       product.quantity -= 1;
+      product.priceNew = product.quantity * product.price;
+      this.calculateTotal();
     }
   }
 
   plus(id: number) {
-    const product = this.product.find((item) => item.id === id);
-    if (product && product.quantity) {
-      if (product.quantity < 9) {
-        product.quantity += 1;
-      }
+    const product = this.card.products.find((item) => item.id === id);
+    if (
+      product &&
+      product.quantity &&
+      product.quantity < product.quantityProducts
+    ) {
+      product.quantity += 1;
+      product.priceNew = product.quantity * product.price;
+      this.calculateTotal();
     }
   }
 }
