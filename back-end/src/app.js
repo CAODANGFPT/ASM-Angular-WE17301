d92b800f33e4f6db10cf56d26df3a07956c14cee
Phuclvph20069 <phuclvph20069@fpt.edu.vn>
2023-06-08 22:14:41 +0700

    feature: create categories data refs: AAW-17

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,7 +2,9 @@ import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 import authRouter from "./routes/auth";
+import categoryRouter from "./routes/category";
 import productRouter from "./routes/product";
+
 const app = express();
 
 // middleware
@@ -12,6 +14,7 @@ app.use(cors());
 // router
 app.use("/api", productRouter);
 app.use("/api", authRouter);
+app.use("/api", categoryRouter);
 
 mongoose.connect("mongodb://127.0.0.1:27017/ASM-ANGULAR");
 
