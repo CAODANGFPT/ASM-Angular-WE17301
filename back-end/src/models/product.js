af223fdf46ba9749de4334a58bb8991c48b9bb0c
Phuclvph20069 <phuclvph20069@fpt.edu.vn>
2023-06-10 13:00:43 +0700

    fix: data and get api products list page refs: AAW-41

--- back-end/src/models/product.js
+++ back-end/src/models/product.js
@@ -1,27 +1,31 @@
 import mongoose from "mongoose";
-import mongoosePaginate from "mongoose-paginate-v2"
+import mongoosePaginate from "mongoose-paginate-v2";
 const productSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String
-        },
-        price: {
-            type: Number
-        },
-        quantity: {
-            type: Number
-        },
-        image: {
-            type: String
-        },
-        description: {
-            type: String
-        },
-        categoryId: {
-            type: mongoose.Types.ObjectId,
-            ref: "Category",
-        }
-    },{timestamps: true, versionKey: false}
+  {
+    name: {
+      type: String,
+    },
+    priceNew: {
+      type: Number,
+    },
+    priceOld: {
+      type: Number,
+    },
+    quantity: {
+      type: Number,
+    },
+    image: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    categoryId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Category",
+    },
+  },
+  { timestamps: true, versionKey: false }
 );
-productSchema.plugin(mongoosePaginate)
-export default mongoose.model('Product', productSchema);
+productSchema.plugin(mongoosePaginate);
+export default mongoose.model("Product", productSchema);
