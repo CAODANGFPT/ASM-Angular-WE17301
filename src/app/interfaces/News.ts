e772622b927beee89a7869da1f99605990bb305e
buiquynh-123 <quynhbtdph19396@fpt.edu.vn>
2023-06-12 12:15:48 +0700

    feature: get api admin news add pages refs: AAW-50
    Signed-off-by: buiquynh-123 <quynhbtdph19396@fpt.edu.vn>

---
+++ src/app/interfaces/News.ts
@@ -0,0 +1,9 @@
+export interface INews {
+  _id?: string;
+  name: string;
+  image: string;
+  author: string;
+  date: Date;
+  describe: string;
+  description: string;
+}
---
+++ src/app/services/news.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { INews } from '../interfaces/News';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class NewsService {
+  constructor(private http: HttpClient) {}
+  addNews(news: INews): Observable<INews> {
+    return this.http.post<INews>(`http://localhost:8080/api/news`, news);
+  }
+}
