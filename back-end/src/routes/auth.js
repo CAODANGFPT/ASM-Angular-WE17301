30cdf8a91800d828f5a8c871535fbcf8c953cec9
hai20112003 <nguyenhai20112003@gmail.com>
2023-06-12 11:23:10 +0700

    feature: create data user refs: AAW-55

--- back-end/src/routes/auth.js
+++ back-end/src/routes/auth.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { signin } from "../controllers/auth";
+import { get, getAll, signin, update } from "../controllers/auth";
 import { signup } from "../controllers/auth";
 
 const router = express.Router();
 
 router.post("/signin", signin);
 router.post("/signup", signup);
+router.get("/User", getAll);
+router.get("/User/:id", get);
+router.put("/User/:id", update);
+
+
+
 
 export default router;
