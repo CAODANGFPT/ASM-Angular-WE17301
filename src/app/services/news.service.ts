49dc609da667d5eed76d337b955be405cf653b7c
Phuclvph20069 <phuclvph20069@fpt.edu.vn>
2023-06-12 15:30:07 +0700

    feature: remove api admin news page refs: AAW-52

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -25,4 +25,8 @@ export class NewsService {
       news
     );
   }
+  
+  deleteNews(id: number | string): Observable<INews> {
+    return this.http.delete<INews>(`http://localhost:8080/api/news/${id}`);
+  }
 }
